98889eef4f989ba7cb860058b5adb42cbe90af13
quan18070712 <quan.nguyen_bk2017@hcmut.edu.vn>
2021-05-07 23:10:02 +0700

    Design UI Home and Create Slider

--- PhotoEditor/app/src/main/java/com/example/photoeditor/MainActivity.java
+++ PhotoEditor/app/src/main/java/com/example/photoeditor/MainActivity.java
@@ -6,9 +6,13 @@ import android.os.Bundle;
 import android.text.SpannableString;
 import android.text.Spanned;
 import android.text.style.ForegroundColorSpan;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity {
+
+    ViewFlipper v_flipper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,5 +32,28 @@ public class MainActivity extends AppCompatActivity {
         ss.setSpan(fcsGreen, 6, 12, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
 
         textView.setText(ss);
+
+        // Create Slider
+
+        int[] images = {R.drawable.pager_01, R.drawable.pager_02, R.drawable.pager_03};
+
+        v_flipper = findViewById(R.id.v_flipper);
+
+        for (int image: images){
+            flipperImages(image);
+        }
+    }
+
+    public void flipperImages(int image){
+        ImageView imageView = new ImageView(this);
+        imageView.setBackgroundResource(image);
+
+        v_flipper.addView(imageView);
+        v_flipper.setFlipInterval(4000);
+        v_flipper.setAutoStart(true);
+
+        // animation
+        v_flipper.setInAnimation(this, android.R.anim.slide_in_left);
+        v_flipper.setOutAnimation(this, android.R.anim.slide_out_right);
     }
 }
