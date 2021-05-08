d577c5e3a7c3361d09849e8820c1c4c584e2f25b
quan18070712 <quan.nguyen_bk2017@hcmut.edu.vn>
2021-05-08 10:21:49 +0700

    Create Action to Camera, Editor and More Button

--- PhotoEditor/app/src/main/java/com/example/photoeditor/MainActivity.java
+++ PhotoEditor/app/src/main/java/com/example/photoeditor/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.photoeditor;
 
+import android.content.Intent;
 import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
 import android.text.SpannableString;
 import android.text.Spanned;
 import android.text.style.ForegroundColorSpan;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.ViewFlipper;
@@ -13,6 +18,10 @@ import android.widget.ViewFlipper;
 public class MainActivity extends AppCompatActivity {
 
     ViewFlipper v_flipper;
+    Button btnCam;
+    Button btnEdit;
+    Button hregister;
+    private static final int PICK_IMAGE = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +51,31 @@ public class MainActivity extends AppCompatActivity {
         for (int image: images){
             flipperImages(image);
         }
+
+        // Open Camera
+        btnCam = findViewById(R.id.camera);
+        btnCam.setOnClickListener(view -> {
+            try {
+                Intent intent= new Intent();
+                intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivity(intent);
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        });
+
+        // Open Gallery
+        btnEdit = findViewById(R.id.editor);
+        btnEdit.setOnClickListener(v -> openGallery());
+
+        //Link to Website
+        hregister = findViewById(R.id.more);
+        hregister.setOnClickListener(v -> {
+            String url = "https://play.google.com/store/search?q=CHUMOB";
+
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            startActivity(intent);
+        });
     }
 
     public void flipperImages(int image){
@@ -52,8 +86,13 @@ public class MainActivity extends AppCompatActivity {
         v_flipper.setFlipInterval(4000);
         v_flipper.setAutoStart(true);
 
-        // animation
+        // Animation
         v_flipper.setInAnimation(this, android.R.anim.slide_in_left);
         v_flipper.setOutAnimation(this, android.R.anim.slide_out_right);
+    }
+
+    private void openGallery() {
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, PICK_IMAGE);
     }
 }
